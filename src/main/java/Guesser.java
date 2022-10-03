499ced8ba957a7fcccc25b291e901e545f69563f
Masaru Chida <mchida@cs.washington.edu>
2022-10-02 17:24:33 -0700

    Ready to commit and push

--- src/main/java/Guesser.java
+++ src/main/java/Guesser.java
@@ -11,6 +11,44 @@ public class Guesser {
     public static BigInteger findNumber(Chooser c) {
         // Tip: If you're not sure how to work with BigInteger numbers, we encourage
         // you to look up its Javadoc online.
-        throw new RuntimeException("Remove this line and implement me!");
+        BigInteger guess = BigInteger.ONE;
+        BigInteger guessLowerBound = BigInteger.ONE;
+        String response = c.guess(guess);
+
+        while(response.equals("higher")) {
+            guessLowerBound = guess;
+            guess = guess.multiply(BigInteger.TWO);
+            response = c.guess(guess);
+        }
+
+        if(c.guess(guess).equals("correct")) {
+            return guess;
+        }
+
+        guess = BinarySearch(guessLowerBound, guess, c);
+
+        return guess;
+    }
+
+    public static BigInteger BinarySearch(BigInteger lowerBound, BigInteger upperBound, Chooser c) {
+
+        BigInteger mid = (lowerBound.add(upperBound)).divide(BigInteger.TWO);
+
+        if (upperBound.compareTo(lowerBound) < 0) {
+            return BigInteger.valueOf(-1);
+        }
+
+        String response = c.guess(mid);
+
+        if (response.equals("higher")) {
+            lowerBound = mid.add(BigInteger.ONE);
+            return BinarySearch(lowerBound, upperBound, c);
+        } else if (response.equals("lower")) {
+            upperBound = mid.subtract(BigInteger.ONE);
+            return BinarySearch(lowerBound, upperBound, c);
+        }
+
+        return  mid;
+
     }
 }
